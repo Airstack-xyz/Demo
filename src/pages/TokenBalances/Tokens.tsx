d64be6bc6940ff9ec383ceca0c67182ea7bd5a81
Aadil Hasan <aadil@airstack.xyz>
2023-07-12 13:08:34 +0530

    responsiveness

--- src/pages/TokenBalances/Tokens.tsx
+++ src/pages/TokenBalances/Tokens.tsx
@@ -121,7 +121,7 @@ export function Tokens() {
     : [...tokens, ...poaps];
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-3 gap-11 mt-3.5 w-full justify-items-center">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-11 mt-3.5 w-full justify-items-center">
       {items.map((_token, index) => {
         const token = _token as TokenType;
         const poap = _token as Poap;
