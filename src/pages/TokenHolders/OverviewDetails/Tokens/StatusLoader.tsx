5896ed1a28d916eb5e581f542184060bd8de9f6f
Aadil Hasan <aadil@airstack.xyz>
2023-07-28 22:59:29 +0530

    complete status loader

--- src/pages/TokenHolders/OverviewDetails/Tokens/StatusLoader.tsx
+++ src/pages/TokenHolders/OverviewDetails/Tokens/StatusLoader.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import { useSearchInput } from '../../../../hooks/useSearchInput';
+import { useMemo } from 'react';
 
 type LoaderProps = {
   total: number;
@@ -8,18 +9,26 @@ type LoaderProps = {
 
 export function StatusLoader({ total, matching }: LoaderProps) {
   const [{ activeViewToken }] = useSearchInput();
+  const loader = useMemo(() => {
+    return (
+      <img src="images/loader.svg" height={20} width={30} className="mr-2" />
+    );
+  }, []);
   return (
-    <div className="fixed inset-10 flex justify-center items-end pointer-events-none">
-      <div className="bg-glass rounded-18 p-9 border-solid-stroke">
-        <div className="mb-4">
-          Scanning {total} {activeViewToken}..
+    <div className="fixed inset-5 sm:inset-10 flex justify-center items-end pointer-events-none">
+      <div className="bg-glass rounded-18 p-9 border-solid-stroke max-w-[90%] sm:max-w-[500px]">
+        <div className="mb-4 flex items-center">
+          {loader}
+          <span className="ellipsis">
+            Scanning {total} {activeViewToken}
+          </span>
         </div>
         <div
-          className={classNames({
+          className={classNames('flex ellipsis', {
             'text-text-secondry': !matching
           })}
         >
-          {matching ? 'Found' : 'Find'} {matching} matching results
+          {loader} {matching ? 'Found' : 'Find'} {matching} matching results
         </div>
       </div>
     </div>
