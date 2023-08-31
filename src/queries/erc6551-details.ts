48c58d6dd2d5b3b0779ef5a4984fbdd3777c15bd
Aadil Hasan <aadil@airstack.xyz>
2023-08-31 11:20:20 +0530

    WIP details

--- src/queries/erc6551-details.ts
+++ src/queries/erc6551-details.ts
@@ -1,34 +1,67 @@
 export const erc6551DetailsQuery = `query ERC6551Details($blockchain: TokenBlockchain!, $tokenAddress: Address, $tokenId: String) {
-    Accounts(
-      input: {blockchain: $blockchain, filter: {tokenAddress: {_eq: $tokenAddress}, tokenId: {_eq: $tokenId}}}
-    ) {
-      Account {
-        deployer
-        address {
-          addresses
-          tokenBalances {
-            tokenType
-            tokenAddress
-            tokenId
-            owner {
-              identity
-            }
-            tokenNfts {
-              lastTransferTimestamp
-              lastTransferBlock
-              lastTransferHash
-              tokenURI
-              token {
-                tokenTraits
-              }
-              erc6551Accounts {
-                address {
-                  addresses
-                }
+  Accounts(
+    input: {blockchain: $blockchain, filter: {tokenAddress: {_eq: $tokenAddress}, tokenId: {_eq: $tokenId}}}
+  ) {
+    Account {
+      nft {
+        lastTransferTimestamp
+        lastTransferBlock
+        lastTransferHash
+        tokenURI
+        tokenId
+        tokenBalances {
+          tokenType
+        }
+        token {
+          name
+          symbol
+          tokenTraits
+          totalSupply
+          owner {
+            identity
+          }
+        }
+      }
+      address {
+        tokenBalances {
+          tokenType
+          tokenAddress
+          tokenId
+          owner {
+            identity
+          }
+          tokenNfts {
+            erc6551Accounts {
+              address {
+                addresses
               }
             }
           }
         }
       }
     }
-  }`;
+  }
+}`;
+
+export const tokenDetailsQuery = `query MyQuery($tokenAddress: Address, $tokenId: String, $blockchain: TokenBlockchain!, $limit: Int) {
+  TokenBalances(
+    input: {filter: {tokenAddress: {_eq: $tokenAddress}, tokenId: {_eq: $tokenId}}, blockchain: $blockchain, limit: $limit}
+  ) {
+    TokenBalance {
+      owner {
+        identity
+      }
+      amount
+      tokenAddress
+      tokenId
+      tokenType
+      tokenNfts {
+        contentValue {
+          image {
+            medium
+          }
+        }
+      }
+    }
+  }
+}`;
