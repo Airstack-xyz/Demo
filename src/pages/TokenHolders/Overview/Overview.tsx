8f88fe6124f88e8c720e7116d6a06f28d79b4b5e
Aadil Hasan <aadil@airstack.xyz>
2023-09-08 16:43:33 +0530

    fix 6551 details page action not taking to account

--- src/pages/TokenHolders/Overview/Overview.tsx
+++ src/pages/TokenHolders/Overview/Overview.tsx
@@ -34,6 +34,16 @@ function Overview({ onAddress404 }: { onAddress404?: () => void }) {
   const hasNoTokens = !loadingTokens && tokens && tokens.length === 0;
   const addressIsAccount =
     hasNoTokens && !loadingAccount && Boolean(account?.tokenAddress);
+  const tokenWith6551 = useMemo(() => {
+    if (!account) return null;
+    const { tokenAddress, tokenId, blockchain } = account;
+    return {
+      tokenAddress,
+      tokenId,
+      blockchain,
+      eventId: ''
+    };
+  }, [account]);
 
   useEffect(() => {
     if (hasNoTokens && onAddress404) {
@@ -296,7 +306,7 @@ function Overview({ onAddress404 }: { onAddress404?: () => void }) {
     return (
       <ERC6551TokenHolder
         owner={account?.token?.owner?.identity}
-        token={account?.token}
+        token={tokenWith6551 || account?.token}
       />
     );
   }
