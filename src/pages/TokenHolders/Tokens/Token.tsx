4b5d66462c2add830d8661819d82d69ed727fbce
kaushalmeena <kaushal.meena1996@gmail.com>
2023-09-22 15:57:18 +0530

    fix search submit blur issue

--- src/pages/TokenHolders/Tokens/Token.tsx
+++ src/pages/TokenHolders/Tokens/Token.tsx
@@ -28,7 +28,7 @@ export function Token({
     : '';
   const tokenId = tokenInProps?.tokenId || '';
   const tokenAddress = tokenInProps?.tokenAddress || '';
-  const primarEns = owner?.primaryDomain?.name || '';
+  const primaryEns = owner?.primaryDomain?.name || '';
   const ens = owner?.domains?.map(domain => domain.name) || [];
   const token = tokenInProps as TokenType;
   const poap = tokenInProps as Poap;
@@ -228,7 +228,7 @@ export function Token({
       <td className="ellipsis">
         {}
         <ListWithMoreOptions
-          list={[primarEns || '']}
+          list={[primaryEns || '']}
           onShowMore={getShowMoreHandler(ens, 'ens')}
           listFor="ens"
           onItemClick={handleAddressClick}
