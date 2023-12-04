242246076a86986c854c3ecd84337c798af6da39
kaushalmeena <kaushal.meena1996@gmail.com>
2023-12-04 20:08:59 +0530

    fix date formatting

--- src/Components/Filters/AllFilters.tsx
+++ src/Components/Filters/AllFilters.tsx
@@ -263,9 +263,12 @@ export function AllFilters({
       case 'customDate':
         {
           const date = customDate as Date;
-          const dateString = `${date.getFullYear()}-${
-            date.getMonth() + 1
-          }-${date.getDate()}`;
+
+          const yyyy = date.getFullYear();
+          const mm = String(date.getMonth() + 1).padStart(2, '0');
+          const dd = String(date.getDate()).padStart(2, '0');
+          const dateString = `${yyyy}-${mm}-${dd}`;
+
           snapshotData.customDate = dateString;
         }
         break;
