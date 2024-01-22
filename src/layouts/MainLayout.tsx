213154fe897d235a3304188cfc5c6882f21b7c71
kaushalmeena <kaushal.meena1996@gmail.com>
2024-01-22 13:28:47 +0530

    chore: move tags in index.html

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,35 +1,5 @@
 import { ReactNode } from 'react';
 import { Header } from '../Components/Header';
-import { Helmet } from 'react-helmet';
-
-const BASE_URL = process.env.BASE_URL as string;
-
-function BaseMetaData() {
-  const title = 'Airstack Explorer';
-  const description =
-    'Visual Blockchain Explorer. Open source. Forkable. Fetch identities including; ENS, Lens, Farcaster, XMTP, POAPs, NFTs, Tokens.';
-  const image = `${BASE_URL}/images/open-graph/explorer.png`;
-
-  return (
-    <Helmet>
-      <title>{title}</title>
-      <meta name="description" content={description} />
-
-      <meta property="og:url" content={BASE_URL} />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
-
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@airstack_xyz" />
-      <meta name="twitter:url" content={BASE_URL} />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image} />
-    </Helmet>
-  );
-}
 
 export function MainLayout({
   children
@@ -39,7 +9,6 @@ export function MainLayout({
 }) {
   return (
     <div className="pt-[70px] pb-8 max-sm:min-h-[140vh]">
-      <BaseMetaData />
       <Header />
       <div className="">{children}</div>
     </div>
--- src/pages/OnchainGraph/index.tsx
+++ src/pages/OnchainGraph/index.tsx
@@ -1,46 +1,14 @@
 import classNames from 'classnames';
-import { Helmet } from 'react-helmet';
 import { GetAPIDropdown } from '../../Components/GetAPIDropdown';
-import { MainLayout } from '../../layouts/MainLayout';
 import { MAX_SEARCH_WIDTH } from '../../Components/Search/constants';
+import { MainLayout } from '../../layouts/MainLayout';
 import { OnChainGraph } from './OnChainGraph';
 import { Search } from './Search';
-import { useIdentity } from './hooks/useIdentity';
 import { getAPIDropdownOptions } from './constants';
-
-const BASE_URL = process.env.BASE_URL as string;
-
-function MetaData() {
-  const identity = useIdentity();
-
-  const url = `${BASE_URL}/onchain-graph?identity=${identity}`;
-  const title = 'Airstack Onchain Graph';
-  const description =
-    'The onchain address book - analyze onchain interactions to create recommendations.';
-  const image = `${BASE_URL}/images/open-graph/onchain-graph.png`;
-
-  return (
-    <Helmet>
-      <title>{title}</title>
-      <meta name="description" content={description} />
-
-      <meta property="og:url" content={url} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
-
-      <meta name="twitter:url" content={url} />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image} />
-    </Helmet>
-  );
-}
 
 export function OnChainGraphPage() {
   return (
     <MainLayout>
-      <MetaData />
       <div className={classNames('px-2 pt-5 max-w-[1440px] mx-auto sm:pt-8')}>
         <div style={{ maxWidth: MAX_SEARCH_WIDTH }} className="mx-auto w-full">
           <Search />
