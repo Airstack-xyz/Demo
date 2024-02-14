a3027660c6dc25bc1898ae4d08316bec2ea92836
Aadil Hasan <aadil@airstack.xyz>
2024-02-14 13:18:04 +0530

    move credits to download text to next line

--- src/Components/CSVDownload/CSVDownloads.tsx
+++ src/Components/CSVDownload/CSVDownloads.tsx
@@ -576,15 +576,17 @@ export function CSVDownloads() {
                     {status === Status.Completed && (
                       <div className="mt-2">
                         <div className="mb-2">
-                          {formatBytes(option.fileSize || 0, 2)} •{' '}
-                          {option.totalRows} rows •{' '}
-                          <span className="text-stroke-highlight-blue">
+                          <div className="mb-1.5 text-text-secondary">
+                            {formatBytes(option.fileSize || 0, 2)} •{' '}
+                            {option.totalRows} rows
+                          </div>
+                          <div className="text-stroke-highlight-blue">
                             {formatNumber(
                               !option.totalRows ? 0 : option.creditsUsed || 0,
                               2
                             )}{' '}
                             credits to download
-                          </span>
+                          </div>
                         </div>
                         <div>
                           {option.totalRows ? (
@@ -596,7 +598,7 @@ export function CSVDownloads() {
                               }}
                             >
                               Download CSV ($
-                              {formatNumber(option.creditPrice || 0, 4)})
+                              {formatNumber(option.creditPrice || 0)})
                             </button>
                           ) : null}
 
