56e566d5e33817f6afd4f781da55b5d7fa366a88
Aadil Hasan <aadil@airstack.xyz>
2024-02-29 20:34:36 +0530

    fix close icon not visible for in-progress tasks

--- src/Components/CSVDownload/CSVDownloads.tsx
+++ src/Components/CSVDownload/CSVDownloads.tsx
@@ -599,20 +599,19 @@ export function CSVDownloads() {
                           {option.label}
                         </span>
                       </div>
-                      {isInProgress ||
-                        (largeFile && (
-                          <div className="flex items-center ml-2 min-w-[20px]">
-                            <button>
-                              <Icon
-                                name="cancel-circle"
-                                onClick={e => {
-                                  e.preventDefault();
-                                  setTaskToCancel(option.id);
-                                }}
-                              />
-                            </button>
-                          </div>
-                        ))}
+                      {(isInProgress || largeFile) && (
+                        <div className="flex items-center ml-2 min-w-[20px]">
+                          <button>
+                            <Icon
+                              name="cancel-circle"
+                              onClick={e => {
+                                e.preventDefault();
+                                setTaskToCancel(option.id);
+                              }}
+                            />
+                          </button>
+                        </div>
+                      )}
                     </div>
                     <div className="ml-5">
                       {status === Status.Completed && (
