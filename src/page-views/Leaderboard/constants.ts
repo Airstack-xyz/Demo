46814e50a53513c3a4462387d60a254709018726
Aadil Hasan <aadil@airstack.xyz>
2024-03-20 01:00:53 +0530

    hide not allowed profiles

--- src/page-views/Leaderboard/constants.ts
+++ src/page-views/Leaderboard/constants.ts
@@ -1,7 +1,6 @@
-// const ids = [
-//   602, 19865, 3761, 289309, 9613, 16904, 9367, 8408, 17835, 8092, 11536, 8093,
-//   19851, 15071, 15971, 2602, 6806, 11161, 8107, 19854
-// ];
+const ids = [
+  602, 19865, 3761, 289309, 9613, 16904, 9367, 8408, 17835, 8092, 11536, 8093,
+  19851, 15071, 15971, 2602, 6806, 11161, 8107, 19854
+];
 
-// TODO: use the ids to create a Set of allowed farcaster ids
-export const noAllowedFarcasterIds = new Set<number>([]);
+export const noAllowedFarcasterIds = new Set<number>(ids);
