75c086d6e585c83df1c7c310db171e2c6db2f840
Aadil Hasan <aadil@airstack.xyz>
2024-03-20 00:39:06 +0530

    temp allow all profiles

--- src/page-views/Leaderboard/constants.ts
+++ src/page-views/Leaderboard/constants.ts
@@ -1,5 +1,7 @@
-const ids = [
-  602, 19865, 3761, 289309, 9613, 16904, 9367, 8408, 17835, 8092, 11536, 8093,
-  19851, 15071, 15971, 2602, 6806, 11161, 8107, 19854
-];
-export const noAllowedFarcasterIds = new Set(ids);
+// const ids = [
+//   602, 19865, 3761, 289309, 9613, 16904, 9367, 8408, 17835, 8092, 11536, 8093,
+//   19851, 15071, 15971, 2602, 6806, 11161, 8107, 19854
+// ];
+
+// TODO: use the ids to create a Set of allowed farcaster ids
+export const noAllowedFarcasterIds = new Set<number>([]);
