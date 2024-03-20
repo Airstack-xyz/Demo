a671c487131e1e944bf0008b0b517e5e97c3d4d0
Aadil Hasan <aadil@airstack.xyz>
2024-03-20 19:45:51 +0530

    hide only one profile for now

--- src/page-views/Leaderboard/constants.ts
+++ src/page-views/Leaderboard/constants.ts
@@ -1,6 +1,7 @@
-const ids = [
-  602, 19865, 3761, 289309, 9613, 16904, 9367, 8408, 17835, 8092, 11536, 8093,
-  19851, 15071, 15971, 2602, 6806, 11161, 8107, 19854
-];
+// const ids = [
+//   602, 19865, 3761, 289309, 9613, 16904, 9367, 8408, 17835, 8092, 11536, 8093,
+//   19851, 15071, 15971, 2602, 6806, 11161, 8107, 19854
+// ];
 
-export const noAllowedFarcasterIds = new Set<number>(ids);
+// for now only 602 is not allowed, add above ids to not allow list later
+export const noAllowedFarcasterIds = new Set<number>([602]);
